ac26a4203104fa005c36a13d1e24437cfab2b0e6
Senthil Kumaran <senthilx@amazon.com>
2023-06-20 12:57:55 -0700

    updated tertiary options.

--- src/data/tertiary-options.js
+++ src/data/tertiary-options.js
@@ -89,7 +89,7 @@ export const tertiaryOptions = {
             value: 'custom-kubelet-configuration',
             label: 'custom kubelet configuration',
             usage: 'eksctl create cluster -f cluster.yaml',
-            nb: 'The values specified in the config file for the\nthe fields in kubeletExtraconfig will completely\noverwrite the default values specified by eksctl.\nConfig files can include the kubeletExtraConfig\nfield which accepts a free form yaml that will be\nembedded into the kubelet.yaml.',
+            nb: 'The values specified in the config file for the the fields\nin kubeletExtraconfig will completely overwrite the default\nvalues specified by eksctl. Config files can include the\nkubeletExtraConfig field which accepts a free form yaml that\nwill be embedded into the kubelet.yaml.',
             filecontent: 'apiVersion: eksctl.io/v1alpha5\nkind: ClusterConfig\n\nmetadata:\n  name: dev-cluster-1\n  region: eu-north-1\n\nnodeGroups:\n  - name: ng-1\n    instanceType: m5a.xlarge\n    desiredCapacity: 1\n    kubeletExtraConfig:\n      kubeReserved:\n        cpu: "300m"\n        memory: "300Mi"\n        ephemeral-storage: "1Gi"\n      kubeReservedCgroup: "/kube-reserved"\n      systemReserved:\n        cpu: "300m"\n        memory: "300Mi"\n        ephemeral-storage: "1Gi"\n      evictionHard:\n        memory.available:  "200Mi"\n        nodefs.available: "10%"\n      featureGates:\n        RotateKubeletServerCertificate: true # has to be enabled, otherwise it will be disabled'
         }
     ],
--- tools/textwrap-string.py
+++ tools/textwrap-string.py
@@ -4,4 +4,4 @@ msg = """
 The values specified in the config file for the the fields in kubeletExtraconfig will completely overwrite the default values specified by eksctl. Config files can include the kubeletExtraConfig field which accepts a free form yaml that will be embedded into the kubelet.yaml.
 """
 
-print(repr("\n".join(textwrap.wrap(msg, width=50))))
+print(repr("\n".join(textwrap.wrap(msg, width=60))))
