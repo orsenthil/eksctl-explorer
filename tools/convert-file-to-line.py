5e902c6da9339e8159cb93be28c4418eeb485397
Senthil Kumaran <senthilx@amazon.com>
2023-06-22 19:00:13 -0700

    Cloudwatch logging.

--- tools/convert-file-to-line.py
+++ tools/convert-file-to-line.py
@@ -1,7 +1,8 @@
 import os
 
+cluster_yaml = "/Users/senthilx/go/src/github.com/weaveworks/eksctl/examples/01-simple-cluster.yaml"
 
-with open("/Users/senthilx/cluster.yaml") as f:
+with open(cluster_yaml) as f:
     data = f.readlines()
 
 print(repr("".join(data)))
--- tools/textwrap-string.py
+++ tools/textwrap-string.py
@@ -2,10 +2,9 @@ import textwrap
 
 
 
-msg = """
-Amazon EKS supports IAM Roles for Service Accounts (IRSA) that allows cluster operators to map AWS IAM Roles to Kubernetes Service Accounts.
-This provides fine-grained permission management for apps that run on EKS and use other AWS services. These could be apps that use S3, any other data services (RDS, MQ, STS, DynamoDB), or Kubernetes components like AWS Load Balancer controller or ExternalDNS.
-This demonstrates how to use IRSA with a config file that attaches different policies to different service accounts.
+msg = """CloudWatch logging for EKS control plane is not enabled by default due to data ingestion and storage costs.
+To enable control plane logging when cluster is created, you will need to define cloudWatch.clusterLogging.enableTypes setting in your ClusterConfig.
 """
+
 
 print(repr("\n".join(textwrap.wrap(msg, width=60))))
