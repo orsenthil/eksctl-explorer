3fecd0b73c517cf04787094770e10b0f68cc170e
Senthil Kumaran <senthilx@amazon.com>
2023-06-22 23:29:29 -0700

    updated with pre.

--- src/App.js
+++ src/App.js
@@ -265,9 +265,11 @@ class App extends Component {
                   {nb ? (
                     <div className="board__group board__group--2">
                       <h2 className="board__title  dark-white">Note</h2>
-                      <div className="board board--3">
-                            {nb}
-                      </div>
+                      <pre>
+                        <div className="board board--3">
+                              {nb}
+                        </div>
+                      </pre>
                     </div>
                   ) : null}
 
