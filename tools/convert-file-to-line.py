aed660f631ca35f0020b53c3d2adc0127be06fc7
Senthil Kumaran <senthilx@amazon.com>
2023-07-29 08:41:10 -0700

    Changing the font weight.

--- tools/convert-file-to-line.py
+++ tools/convert-file-to-line.py
@@ -1,6 +1,6 @@
 import os
 
-cluster_yaml = "/Users/senthilx/go/src/github.com/weaveworks/eksctl/examples/01-simple-cluster.yaml"
+cluster_yaml = "/Users/senthilx/go/src/github.com/weaveworks/eksctl/examples/05-advanced-nodegroups.yaml"
 
 with open(cluster_yaml) as f:
     data = f.readlines()
